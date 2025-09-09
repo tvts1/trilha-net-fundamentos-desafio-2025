af660909a41f5c231ed9679d389c5e1bac27627f
TassioTristao <tassio.tristao05@gmail.com>
2025-09-09 09:50:48 -0300

    feat: Add interactive console menu for Parking

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using parking.Models;
+
+Console.Write("Valor inicial do estacionamento: R$ ");
+var initialPrice = float.Parse(Console.ReadLine());
+
+Console.Write("Valor por hora: R$ ");
+var pricePerHour = float.Parse(Console.ReadLine());
+
+var parking = new Parking(initialPrice, pricePerHour);
+
+var menu = new Menu(parking);
+menu.Show();
